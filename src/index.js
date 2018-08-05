b5dbb39056a44e312e92cefd4594696db351076d
donkeykong91 <darylnator@gmail.com>
2018-08-05 01:08:44 -0700

    Moved index.js from dist to src

---
+++ src/index.js
@@ -0,0 +1,49 @@
+const { render } = ReactDOM;
+
+
+const style = {
+
+
+  backgroundColor: "orange",
+
+  color: "white",
+
+  fontFamily: "verdana"
+
+
+};
+
+
+render(
+
+
+  <h1 id="title" className="header"
+
+    style=
+
+    {
+
+      {
+
+
+        backgroundColor: "orange",
+
+        color: "white",
+
+        fontFamily: "verdana"
+
+
+      }
+
+    }
+
+  >
+
+    Hello World
+
+  </h1>,
+
+  document.getElementById("react-container")
+
+
+);
