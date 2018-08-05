0489ac0a9f82931112524c17b525a74977428a52
donkeykong91 <darylnator@gmail.com>
2018-08-05 05:05:33 -0700

    Added file to use contents of titles.json

---
+++ src/lib.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import text from "titles.json";
+
+
+export const hello = {
+
+  <h1 id="title" className="header" style={
+
+    {
+
+      backgroundColor: "purple",
+
+      color: "yellow"
+
+    }
+
+  }>
+
+    {text.hello}
+
+  </h1>
+
+}
