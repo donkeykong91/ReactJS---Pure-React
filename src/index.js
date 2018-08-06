f1978841ca984ab0f73518527c4e883fc3d73aff
donkeykong91 <darylnator@gmail.com>
2018-08-06 13:45:46 -0700

    Added SkiDayCount from component file and added <SkiDayCount /> as first argument in render

--- src/index.js
+++ src/index.js
@@ -2,23 +2,16 @@ import React from "react";
 
 import { render } from "react-dom";
 
-import { hello, goodbye } from "./lib";
+import { SkiDayCount } from "./components/SkiDayCount";
+
+
+window.React = React;
 
 
 render(
 
-
-  <div>
-
-
-    {hello}
-
-    {goodbye}
-
-
-  </div>,
+  <SkiDayCount />,
 
   document.getElementById("react-container")
-
 
 );
