9d382810a61f367899491f518454d47dc74c218b
donkeykong91 <darylnator@gmail.com>
2018-08-07 11:46:14 -0700

    Added table rows for four properties to display

--- src/components/SkiDayRow.js
+++ src/components/SkiDayRow.js
@@ -3,3 +3,42 @@ import { MdTerrain } from "react-icons/md";
 import { TiWeatherSnow } from "react-icons/ti";
 
 import { FaCalendarAlt } from "react-icons/fa";
+
+
+export const SkiDayRow = function ({resort, date, powder, backcountry }) {
+
+  return (
+
+    <tr>
+
+
+      <td>
+
+        {date.getMonth() + 1}/{date.getDate()}/{date.getFullYear()}
+
+      </td>
+
+      <td>
+
+        {resort}
+
+      </td>
+
+      <td>
+
+        {(powder) ? <TiWeatherSnow /> : null}
+
+      </td>
+
+      <td>
+
+        {(backcountry) ? <MdTerrain /> : null}
+
+      </td>
+
+
+    </tr>
+
+  );
+
+};
