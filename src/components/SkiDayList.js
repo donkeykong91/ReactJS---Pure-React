778f17a3b4489ee5d8b1b5735bd03dc335490106
donkeykong91 <darylnator@gmail.com>
2018-08-08 10:08:55 -0700

    Added proptypes to check if days is an array and other conditions as well

--- src/components/SkiDayList.js
+++ src/components/SkiDayList.js
@@ -6,6 +6,8 @@ import { FaCalendarAlt } from "react-icons/fa";
 
 import { SkiDayRow } from "./SkiDayRow";
 
+import { PropTypes } from "react";
+
 
 export const SkiDayList = function ({days}) {
 
@@ -81,5 +83,36 @@ export const SkiDayList = function ({days}) {
 
 
   );
+
+}
+
+
+SkiDayList.propTypes = {
+
+  days: function(props) {
+
+    if (!Array.isArray(props.days)) {
+
+      return new Error(
+
+        "SkiDayList should be an array"
+
+      );
+
+    } else if (!props.days.length) {
+
+      return new Error(
+
+        "SkyDayList must have at least one record"
+
+      );
+
+    } else {
+
+      return null;
+
+    }
+
+  }
 
 }
