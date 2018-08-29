c6c8f1e9902722917926cd82dc2c36d13fc686e7
donkeykong91 <darylnator@gmail.com>
2018-08-29 04:32:08 -0700

    Added the main app that will render all components

---
+++ src/components/App.js
@@ -0,0 +1,106 @@
+import { createClass } from "react";
+
+import { SkiDayList } from "./SkiDayList";
+
+import { SkiDayCount } from "./SkiDayCount";
+
+
+export const App = createClass({
+
+  getInitialState() {
+
+    return {
+
+      allSkiDays: [
+
+
+          {
+
+            resort: "Squaw Valley",
+
+            date: new Date("1/2/2016"),
+
+            powder: true,
+
+            backcountry: false
+
+          },
+
+          {
+
+            resort: "Kirkwood",
+
+            date: new Date("3/28/2016"),
+
+            powder: false,
+
+            backcountry: false
+
+          },
+
+          {
+
+            resort: "Mt. Tallac",
+
+            date: new Date("4/2/2016"),
+
+            powder: false,
+
+            backcountry: true
+
+          }
+
+
+      ]
+
+    }
+
+  },
+
+
+  countDays(filter) {
+
+    return this.state.allSkiDays.filter(function (day) {
+
+      if(filter) {
+
+        return day[filter];
+
+      } else {
+
+        return day;
+
+      }
+
+    }).length;
+
+  },
+
+
+  render() {
+
+    return (
+
+      <div className="app">
+
+
+        <SkiDayList days={this.state.allSkiDays}/>
+
+        <SkiDayCount
+
+          total={}
+
+          powder={}
+
+          backcountry={}
+
+        />
+
+
+      </div>
+
+    );
+
+  }
+
+});
