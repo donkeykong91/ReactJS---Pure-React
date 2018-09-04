47eaadacf7c6f3992a2849eee67f58bd8d6670e2
donkeykong91 <darylnator@gmail.com>
2018-09-04 03:01:41 -0700

    Added a component that renders all the members individually

---
+++ src/components/MemberList.js
@@ -0,0 +1,123 @@
+import { Component } from 'react'
+
+import fetch from 'isomorphic-fetch'
+
+import Member from "./Member";
+
+
+class MemberList extends Component {
+
+    constructor(props) {
+
+        super(props)
+
+        this.state = {
+
+            members: [],
+
+            loading: false
+
+        }
+
+    }
+
+
+    componentDidMount() {
+
+      this.setState({loading:true});
+
+      fetch("https://api.randomuser.me/?nat=US&results=12")
+
+        .then(function (response) {
+
+          return response.json();
+
+        })
+
+        .then(function (json) {
+
+          return json.results;
+
+        })
+
+        .then( (members) =>
+
+          this.setState({
+
+            members,
+
+            loading: false
+
+          })
+
+        );
+
+    }
+
+
+    render() {
+
+        const { members, loading } = this.state;
+
+        return (
+
+            <div className="member-list">
+
+            	<h1>Society Members</h1>
+
+              {(loading) ?
+
+                <span>
+
+                  loading...
+
+                </span> :
+
+                <span>
+
+                  {members.length} members
+
+                </span>
+
+              }
+
+              {(members.length) ?
+
+                members.map( function (member, index) {
+
+                  return (
+
+                    <Member
+
+                      key={index}
+
+                      name={member.name.first + " " + member.name.last}
+
+                      email={member.email}
+
+                      thumbnail={member.picture.thumbnail}
+
+                    />
+
+                  );
+
+                }):
+
+                <span>
+
+                  Currently 0 Members
+
+                </span>
+
+              }
+
+            </div>
+
+        )
+
+   }
+
+}
+
+
+export default MemberList
